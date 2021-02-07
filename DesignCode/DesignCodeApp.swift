d665869e0df40a156fc5b70a7aa45d98716806c1
Thiago Santiago <thiagoalexandres@gmail.com>
2021-02-07 14:50:30 -0300

    Initial commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Thiago Santiago on 06/12/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
